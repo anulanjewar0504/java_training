ca7d5f55494a2fe6b74f88a29b052963ba01db40
Anurag Lanjewar <85513169+anulanjewar0504@users.noreply.github.com>
2024-07-11 19:28:19 +0530

    day 15
    Practice.

---
+++ practice/logical/day15/Program5.java
@@ -0,0 +1,11 @@
+package com.jbk.practice.logical.day15;
+
+public class Program5 {
+	public static void main(String[] args) {
+		double b =0;
+		for(int i =1 ; i<=5;i++) {
+			b = Math.pow(i, 4);
+		}
+		System.out.println(b);
+	}
+}
