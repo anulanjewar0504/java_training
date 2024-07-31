20d740d83f4f44e881558e6e09ad392111c11b1b
Anurag Lanjewar <85513169+anulanjewar0504@users.noreply.github.com>
2024-07-31 20:51:56 +0530

    day 17

---
+++ day17Linkedhashmap/Treemp.java
@@ -0,0 +1,10 @@
+package com.jbk.day17Linkedhashmap;
+
+public class Treemp {
+	public static void main(String[] args) {
+		Product p1 = new Product("Mobile", 80000);
+		Product p2 = new Product("Laptop", 50000);
+		Product p3 = new Product("")
+		
+	}
+}
